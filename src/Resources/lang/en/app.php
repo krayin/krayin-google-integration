6e8a72489fcd44f735b76352e9e3634244beb44f
prateek srivastava <prateek.srivastava781@webkul.com>
2021-10-13 11:54:09 +0530

    issue #6 fixed

--- src/Resources/lang/en/app.php
+++ src/Resources/lang/en/app.php
@@ -9,7 +9,7 @@ return [
     'info'                    => 'Google time-management and scheduling calendar for enhancing work speed.',
     'remove'                  => 'Remove',
     'synced-account'          => 'Synced Account',
-    'select-calendar'         => 'Select the calender you want to sync',
+    'select-calendar'         => 'Select the calendar you want to sync',
     'select-calendar-info'    => 'Recurring events from your calendar will not be synced to the CRM',
     'sync-success'            => 'Events synced successfully.',
     'destroy-success'         => 'Google account removed successfully.'
