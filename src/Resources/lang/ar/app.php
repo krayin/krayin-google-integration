1f431a31018e988a3496129ecb55ba6aca7b4ce6
Sanjay <sanjay.bhatt371@webkul.com>
2021-12-23 17:13:10 +0530

    Arabic locale file added

---
+++ src/Resources/lang/ar/app.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'title'                   => 'متصفح الجوجل',
+    'calendar'                => 'تقويم',
+    'meet'                    => 'يجتمع',
+    'back'                    => 'عودة ',
+    'connect-google-calendar' => 'ربط تقويم جوجل',
+    'connect-google-meet'     => 'قم بتوصيل جوجل ميت',
+    'google-calendar'         => 'تقويم جوجل',
+    'google-meet'             => 'جوجل ميت',
+    'google-calendar-info'    => 'إدارة الوقت من Google وجدولة التقويم لتحسين سرعة العمل.',
+    'google-meet-info'        => 'ينشئ روابط لقاء مباشرة في وقت إنشاء النشاط لتحسين سرعة العمل.',
+    'remove'                  => 'إزالة',
+    'synced-account'          => 'حساب متزامن',
+    'select'                  => 'الرجاء التحديد',
+    'select-calendar'         => 'حدد التقويم الذي تريد مزامنته',
+    'select-calendar-info'    => 'لن تتم مزامنة الأحداث المتكررة من التقويم الخاص بك مع CRM',
+    'sync-success'            => 'تمت مزامنة الأحداث بنجاح.',
+    'destroy-success'         => 'تمت إزالة حساب Google بنجاح.',
+    'save-sync'               => 'حفظ ومزامنة',
+    'confirm-remove'          => 'هل أنت متأكد أنك تريد إزالة حساب جوجل؟'
+];
+
+
