ffa75a3b093bf1a53c8e1a9c1493fb6df1221186
suraj kashyap <suraj.kashyap370@webkul.in>
2024-09-25 12:58:27 +0530

    Issue #26 fixed.

---
+++ src/Resources/lang/tr/app.php
@@ -0,0 +1,25 @@
+<?php 
+
+return [
+    'title'                   => 'Google',
+    'calendar'                => 'Takvim',
+    'meet'                    => 'Meet',
+    'back'                    => 'Geri',
+    'view'                    => 'Görünüm',
+    'sync'                    => 'Senkronize Et',
+    'connect-google-calendar' => 'Google Takvimi Bağla',
+    'connect-google-meet'     => 'Google Meet\'i Bağla',
+    'google-calendar'         => 'Google Takvimi',
+    'google-meet'             => 'Google Meet',
+    'google-calendar-info'    => 'İş hızını artırmak için Google zaman yönetimi ve planlama takvimi.',
+    'google-meet-info'        => 'İş hızını artırmak için etkinlik oluşturma sırasında doğrudan Meet bağlantıları oluşturur.',
+    'remove'                  => 'Kaldır',
+    'synced-account'          => 'Senkronize Hesap',
+    'select'                  => 'Lütfen seçin',
+    'select-calendar'         => 'Senkronize etmek istediğiniz takvimi seçin',
+    'select-calendar-info'    => 'Takviminizdeki yinelenen etkinlikler CRM ile senkronize edilmeyecek.',
+    'sync-success'            => 'Etkinlikler başarıyla senkronize edildi.',
+    'destroy-success'         => 'Google hesabı başarıyla kaldırıldı.',
+    'save-sync'               => 'Kaydet ve Senkronize Et',
+    'confirm-remove'          => 'Google hesabını kaldırmak istediğinizden emin misiniz?'
+];
